257551a6062d9139ffadcc0130596aeba5aa8a54
riyadh35 <mdriyadhossain356@gmail.com>
2024-07-29 23:43:45 +0600

    this C Programme Commit

---
+++ commit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    // ------ Single Line Commit -----
+
+    // (single line commit ) = printf ("It's my first commit in c Programme");
+
+    /* --------Double Line Commit ----------*/
+
+    printf ("This is my commit");
+
+    return 0;
+}
