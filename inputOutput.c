888828f3855cdf4cb1e0dfaac6c044519bbfc878
riyadh35 <mdriyadhossain356@gmail.com>
2024-07-30 19:54:07 +0600

    practise

--- inputOutput.c
+++ inputOutput.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+
+
 int main(){
     // Displays the string quatations 
 
@@ -9,11 +11,26 @@ int main(){
     // Now check first get input then show in outputi;
 
 
-    printf("Enter an Interger :");
+    // printf("Enter an Interger :");
 
-    scanf("%d", &testInteger);
+    // scanf("%d", &testInteger);
 
-    printf("Number = %d", testInteger);
+    // printf("Number = %d", testInteger);
+
+    /*Float and Double Input and Output in C Programmer*/
+
+
+
+    float num1;
+    double num2;
+
+    printf("Enter a float number: ");
+    scanf("%f", &num1);
+    printf("Enter another Double number :");
+    scanf("%lf", &num2);
+
+    printf("num1 = %f\n", num1);
+    printf("num2 = %lf\n", num2);
 
 
     return 0;
