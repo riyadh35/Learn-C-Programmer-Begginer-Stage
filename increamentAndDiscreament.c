742fc99b1fb5e23296d03cb8e8dc88752024cea1
riyadh35 <mdriyadhossain356@gmail.com>
2024-07-30 22:48:49 +0600

    Increament And Decreament Operator in C Programme

---
+++ increamentAndDiscreament.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    // Working on Increament And Decreament Operator
+
+    int a = 5;
+
+    printf("increament operator ++a = %d \n", ++a);
+    printf("Decreament Operator a-- = %d \n", --a);
+
+    return 0;
+}
