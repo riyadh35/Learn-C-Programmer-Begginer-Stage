82121178868890b732d378ee23a99c82f6ea85fa
riyadh35 <mdriyadhossain356@gmail.com>
2024-07-31 21:13:24 +0600

    if syntax

--- ifelseStatement.c
+++ ifelseStatement.c
@@ -4,10 +4,13 @@ int main(){
 
     int age = 18;
 
+    // if(test expression) {
+        // code
+    // }
+
     if(age >= 18){
 
-        printf("You are eligible for vote because your age is");
+        printf("You are eligible for vote ");
     }
-
     return 0;
 }
