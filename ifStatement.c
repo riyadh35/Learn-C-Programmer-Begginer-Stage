9b60a7175f82daef062d90ecba0b9521e2eb5441
riyadh35 <mdriyadhossain356@gmail.com>
2024-07-31 21:15:50 +0600

    if Statement

--- ifStatement.c
+++ ifStatement.c
@@ -7,10 +7,16 @@ int main(){
     // if(test expression) {
         // code
     // }
+    // else{
+        //code
+    // }
 
     if(age >= 18){
 
         printf("You are eligible for vote ");
+    }
+    else{
+        printf("You are not eligible for vote.");
     }
     return 0;
 }
