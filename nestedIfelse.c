6f59b1a48b5f73f5f51ae9869d1e1f83e25ef548
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-01 00:50:43 +0600

    Nested if else

---
+++ nestedIfelse.c
@@ -0,0 +1,25 @@
+// it is possible to include if...else statement inside the body of another if...else statement . And this call Nested If Else .
+
+#include <stdio.h>
+
+int main(){
+    int num1, num2;
+
+    printf("Enter two integer: ");
+    scanf("%d %d", &num1, &num2);
+
+    if(num1 >= num2){
+        if(num1 == num2){
+            printf("Result: %d = %d", num1,num2);
+        }
+        else{
+            printf("Result: %d > %d", num1,num2);
+        }
+    }
+    else{
+        printf("Result: %d < %d", num1, num2);
+    }
+
+    return 0;
+
+}
