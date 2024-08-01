da6eb83f5835260b01ea214e6b6027eede58be72
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-02 00:57:32 +0600

    practise

--- sum1to100forloop.c
+++ sum1to100forloop.c
@@ -1,15 +1,16 @@
+// Sum of numbers from 1 to 100
+
 #include <stdio.h>
 
-int main() {
+int main(){
 
     int num , sum = 0;
 
-    for(num = 1; num <= 100; num++){
+    for (num = 1; num <= 100; num++){
         sum+=num;
     }
 
     printf("%d", sum);
-    return 0;
 
-    
+    return 0;
 }
