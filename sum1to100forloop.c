1cfea94566fbe91e0fc27a6900a2a703cff35cdd
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-02 01:18:20 +0600

    Get odd number sum from 1 to 100

--- sum1to100forloop.c
+++ sum1to100forloop.c
@@ -21,7 +21,18 @@ int main(){
         sum1+=num1;
     }
 
-    printf("%d", sum1);
+    printf("%d\n", sum1);
+
+
+    /* Get odd numbur sum from 1 to 100*/
+
+    int num2, sum2 = 0; 
+
+    for (num2 = 1; num2 <=100; num2 = num2 + 2){
+        sum2+=num2;
+    }
+
+    printf("%d\n",sum2);
 
     return 0;
 } 
