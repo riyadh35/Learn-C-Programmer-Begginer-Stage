6fd623acef123ca6ad8b7b2e7ec37e985205f87f
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-03 19:41:48 +0600

    while loop

---
+++ whileloope.c
@@ -0,0 +1,21 @@
+// synax
+
+/*
+    while (testExpression) {
+  // the body of the loop 
+    }
+*/
+
+#include <stdio.h>
+
+int main(){
+
+    int i = 1;
+
+    while (i <= 5){
+        printf("%d\n", i);
+        i++;
+    }
+
+    return 0;
+}
