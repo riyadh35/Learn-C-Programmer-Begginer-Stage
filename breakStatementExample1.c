ee08ca939dd7db513140fc6912e309b87350787a
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-07 00:12:08 +0600

    Continue Statements

--- breakStatementExample1.c
+++ breakStatementExample1.c
@@ -1,26 +1,51 @@
+// #include <stdio.h>
+
+// int main() {
+
+//     int i;
+
+//     double number, sum = 0.0;
+
+//     for(i= 1; i<=10; ++i){
+//         printf("Enter n%d: ", i);
+//         scanf("%lf", &number);
+
+//         //if the user enters a negative number, break the loop
+
+
+//         if(number < 0.0){
+//             break;
+//         }
+//         sum += number; //sum = sum + number;
+//     }
+
+//     printf("Sum = %.2lf", sum);
+
+
+//     return 0;
+// }
+
+
 #include <stdio.h>
 
-int main() {
+int main (){
 
     int i;
 
     double number, sum = 0.0;
 
-    for(i= 1; i<=10; ++i){
-        printf("Enter n%d: ", i);
+    for(i = 1 ; i <= 10 ; ++i){
+        printf("Enter n%d : ", i);
         scanf("%lf", &number);
 
-        //if the user enters a negative number, break the loop
-
-
         if(number < 0.0){
-            break;
+        break;
         }
-        sum += number; //sum = sum + number;
+        sum += number;
     }
+
 
     printf("Sum = %.2lf", sum);
 
-
-    return 0;
+   return 0;
 }
