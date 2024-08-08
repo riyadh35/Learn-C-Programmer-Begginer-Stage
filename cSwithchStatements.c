b2330e849877b35c78e05615e44faf45598f2e1f
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-09 01:00:07 +0600

    calculator

---
+++ cSwithchStatements.c
@@ -0,0 +1,62 @@
+//Syntax
+
+
+// switch (expression)
+// {
+// case /* constant-expression */:
+//     /* code */
+//     break;
+
+// case constant2;
+//     // statements
+//     break;
+//     .
+//     .
+//     .
+
+// default:
+//     break;
+// }
+
+#include <stdio.h>
+
+int main (){
+    
+    char operation;
+
+    double n1, n2;
+
+    printf("Enter an operator (+, - , * , /): ");
+
+    scanf("%c", &operation);
+
+    printf("Enter two operands: ");
+
+    scanf("%lf %lf ", &n1, &n2);
+
+    switch (operation)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", n1, n2, n1+n2);
+        break;
+    case'-':
+        printf("%.1f - %.1lf = %.1lf", n1, n2, n1-n2);
+        break;
+    case'*':
+        printf("%.1lf * %.1lf = %.1lf", n1, n2, n1*n2);
+        break;
+
+    case'/':
+        printf("%.1lf / %.1lf = %.1lf", n1, n2 , n1/n2);
+        break;
+
+    // operator doesn't match an case constant [ +, -, *, / ]
+
+    
+    default:
+        printf("Error! opetor is not correct");
+    }
+
+
+    return 0;
+}
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,3 +1,2 @@
-
-    // printf("Please enter your age: ");
-    // scanf("&d\n", &number);
+     printf("%.1lf / %.1lf = %.1lf", n1, n2 , n1/n2);
+        break;
