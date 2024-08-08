c909d95b97ea25e1de2552a087834be38b3ff457
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-09 01:38:41 +0600

    practise full switch statement

--- switchStatements.c
+++ switchStatements.c
@@ -18,6 +18,12 @@ int main(){
     case '-':
         printf("%.1lf - %.1lf = %1.lf", a1,a2, a1-a2);
         break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", a1,a2, a1*a2);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", a1,a2, a1/a2);
+        break;
     default:
         printf("Sorry ... You are enter a error ...?");
     }
