9a4dadbb21d849d9073e754ed89f896a0f8c0742
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-10 22:50:54 +0600

    print number from 1 to n in loop statements

--- loopRevision.c
+++ loopRevision.c
@@ -4,10 +4,23 @@
 
 int main(){
 
-    int i;
+    int i, n;
 
-    for (i == 1 ; i < 11 ; ++i){
-        printf("%d", i);
+    // for (i == 1 ; i < 11 ; ++i){
+    //     printf("%d", i);
+    // }
+
+    // print natural number from 1 to N using loop #pragma endregion
+
+    printf("Enter any number: ");
+    scanf("%d", &n);
+
+    printf("Natural number print for 1 to %d \n", n);
+
+    for(i == 1; i <= n; ++i){
+        printf("print number %d\n", i);
     }
+
     return 0;
+
 }
