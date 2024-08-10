70dfdd208e78d4f77f29fbd4399323d99f03b9df
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-10 23:18:47 +0600

    program to print natural numbers in range

--- loopRevision.c
+++ loopRevision.c
@@ -4,7 +4,7 @@
 
 int main(){
 
-    int i, n;
+    // int i, n;
 
     // for (i == 1 ; i < 11 ; ++i){
     //     printf("%d", i);
@@ -12,13 +12,32 @@ int main(){
 
     // print natural number from 1 to N using loop #pragma endregion
 
-    printf("Enter any number: ");
-    scanf("%d", &n);
+    // printf("Enter any number: ");
+    // scanf("%d", &n);
 
-    printf("Natural number print for 1 to %d \n", n);
+    // printf("Natural number print for 1 to %d \n", n);
 
-    for(i == 1; i <= n; ++i){
-        printf("print number %d\n", i);
+    // for(i == 1; i <= n; ++i){
+    //     printf("print number %d\n", i);
+    // }
+
+
+    /*
+        Program to print natural numbers in range 
+    */
+
+    int i , start, end;
+
+    printf("Enter start number: ");
+    scanf("%d", &start);
+
+    printf("Enter end number: ");
+    scanf("%d", &end);
+
+    printf("Natural number from %d to %d", start, end);
+
+    for(i == start; i <= end; i++){
+        printf("%d\n", i);
     }
 
     return 0;
