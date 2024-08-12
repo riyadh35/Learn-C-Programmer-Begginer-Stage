4ede36bc0e71826f3c99b8b64781c82a127352bb
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-13 01:01:41 +0600

    goto label

---
+++ goto.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main(){
+
+    
+    char ch;
+    double number1, number2;
+    jump:
+
+    printf("Enter your operator: ");
+    printf("Enter operand: ");
+
+    scanf("%c %lf%lf", &ch, &number1, &number2);
+
+    switch (ch)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", number1, number2, number1 + number2);
+        break;
+    
+    case '-':
+        printf("%.1lf - %.1lf = %.1lf", number1, number2, number1 - number2);
+        break;
+    case '*':
+        printf("%.1lf * %.1lf = %.1lf", number1, number2, number1 * number2);
+        break;
+    case '/':
+        printf("%.1lf / %.1lf = %.1lf", number1, number2, number1/number2);
+        break;
+    default:
+        printf("Opps!! You enter wrond operator. Try Again");
+        goto jump;
+    }
+    return 0;
+}
