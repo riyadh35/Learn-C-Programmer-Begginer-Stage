62756e653f63c6fc8c3f74de5f3d794cfb5d5c91
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-12 23:57:50 +0600

    switch case problem solve

--- switchRevision.c
+++ switchRevision.c
@@ -4,38 +4,61 @@ int main(){
 
       // Write a C Programe to print day of week name using switch case 
 
-      int a;
+    //   int a;
 
-      printf("Enter week day number: ");
-      scanf("%d", &a);
+    //   printf("Enter week day number: ");
+    //   scanf("%d", &a);
 
-      switch (a)
-      {
-        case 1:
-            printf("This is Sunday");
-            break;
-        case 2:
-            printf("This is Monday");
-            break;
-        case 3:
-            printf("This is Tuesday");
-            break;
-        case 4:
-            printf("This is Wednesday");
-            break;
-        case 5:
-            printf("This is Thursday");
-            break;
-        case 6:
-            printf("This is Friday");
-            break;
-        case 7:
-            printf("This is Saturday");
-            break;
+    //   switch (a)
+    //   {
+    //     case 1:
+    //         printf("This is Sunday");
+    //         break;
+    //     case 2:
+    //         printf("This is Monday");
+    //         break;
+    //     case 3:
+    //         printf("This is Tuesday");
+    //         break;
+    //     case 4:
+    //         printf("This is Wednesday");
+    //         break;
+    //     case 5:
+    //         printf("This is Thursday");
+    //         break;
+    //     case 6:
+    //         printf("This is Friday");
+    //         break;
+    //     case 7:
+    //         printf("This is Saturday");
+    //         break;
       
-      default:
-        printf("Error ! You enter out of 7 or bellow of 0");
-      }
+    //   default:
+    //     printf("Error ! You enter out of 7 or bellow of 0");
+    //   }
+
+
+    /*
+     C program to create calculator using switch case and functions
+
+    */
+    char ch;
+    double number1, number2;
+
+    printf("Enter your operator: ");
+    printf("Enter operand: ");
+
+    scanf("%c %lf%lf", &ch, &number1, &number2);
+
+    switch (ch)
+    {
+    case '+':
+        printf("%.1lf + %.1lf = %.1lf", number1, number2, number1 + number2);
+        break;
+    
+    default:
+        printf("Opps!! You enter wrond operator");
+    }
 
     return 0;
 }
