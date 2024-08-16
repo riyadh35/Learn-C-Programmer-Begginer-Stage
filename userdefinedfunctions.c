8d1c09b24fe5964583942d2e29c44367f0f4cba7
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-16 12:39:59 +0600

    C programme Function Syntax

--- userdefinedfunctions.c
+++ userdefinedfunctions.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
-    int addNumbers( int a, int b);         // function prototyep
+
+    void greet(){
+        printf("Good Morning\n");
+    }
 
     int main(){
-
-        int n1,n2,sum;
-
-        printf("Enters two Numbers: ");
-        scanf("%d %d", &n1, &n2);
-
-        sum = addNumbers(n1, n2);              // function call
-        printf("sum = %d", sum);
+        greet();
+        greet();
+        greet();
         return 0;
-    
-    
     }
 
-    int addNumbers(int a, int b){            // function definition
-        int result;
-        result = a + b;
-        return result;                      //return statement
-    }
+
+
+    // int addNumbers( int a, int b);         // function prototyep
+
+    // int main(){
+
+    //     int n1,n2,sum;
+
+    //     printf("Enters two Numbers: ");
+    //     scanf("%d %d", &n1, &n2);
+
+    //     sum = addNumbers(n1, n2);              // function call
+    //     printf("sum = %d", sum);
+    //     return 0;
+    
+    
+    // }
+
+    // int addNumbers(int a, int b){            // function definition
+    //     int result;
+    //     result = a + b;
+    //     return result;                      //return statement
+    // }
