ae81c223ad5bd89b608c5fd2fd1f6e27ae01181b
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-17 00:09:53 +0600

    Argument PassedBut No Return Value

--- typesofUserDefienedfunctions.c
+++ typesofUserDefienedfunctions.c
@@ -42,7 +42,7 @@ void checkPrimeNumber(){
 
 //-----No Argument Passed But Returns a value;
 
-
+/* 
 int getInteger();
 
 int main(){
@@ -83,4 +83,52 @@ int getInteger(){
     printf("Enter a positive integer: ");
     scanf("%d", &n);
     return n;
+} */
+
+
+
+
+// ------- Argument Passed But No Return Value;
+
+// Function Declare korle jodi Return type na thake tahole Void diye function call korte hoi;;
+
+
+void checkPrimeNumbers(int n);
+
+int main(){
+    int n;
+    printf("enter a positive integer: ");
+    scanf("%d", &n);
+
+    // n is passed to the function 
+
+    checkPrimeNumbers(n);
+    
+    return 0;
 }
+
+// return type is void meaning doesn't return any value
+
+
+void checkPrimeNumbers(int n){
+    int i, flag = 0;
+
+
+    // 0 and 1 are not prime numbers
+    if(n == 0 || n == 1)
+        flag = 1;
+    
+
+    for( i = 2; i <= n/2; i++){
+        if(n%i == 0 ){
+            flag = 1; 
+            break;
+        }
+    }
+
+    if(flag == 1)
+        printf("%d is not a prime number.", n);
+    else
+        printf("%d is a prime number.", n);
+}
+
