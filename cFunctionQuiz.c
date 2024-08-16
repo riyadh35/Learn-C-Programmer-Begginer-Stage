d005ee2899db9414bab21dbc533320bd0cc53582
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-16 22:27:39 +0600

    multipications with function in c programme return type

--- cFunctionQuiz.c
+++ cFunctionQuiz.c
@@ -1,12 +1,33 @@
 #include <stdio.h>
 
-void numberMultification(int num1, int num2){
+
+
+// Multipication without return type ;
+/* void numberMultification(int num1, int num2){
     int multification = num1 * num2;
     printf("mulfications %d * %d = %d", num1 , num2, multification);
 }
 
 int main(){
     numberMultification(3,4);
+
+    return 0;
+} */
+
+
+//***************************** */
+// Multipications with return type
+
+
+int numberMultiplications (int num1, int num2){
+    int multifications = num1 * num2;
+
+    return multifications;
+}
+
+int main(){
+    int result = numberMultiplications(4, 4);
+    printf("The multipications result is = %d", result);
 
     return 0;
 }
