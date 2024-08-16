441d3341c39676b7641e910229cc0c0e871a5564
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-16 13:48:03 +0600

    Standard Library Functions

---
+++ standardLibraryFunctions.c
@@ -0,0 +1,11 @@
+
+// Standard library functions 
+
+#include <stdio.h>    // stdio.h --> standardlibrary Functions
+#include <math.h>
+
+int main(){
+        float result = sqrt(25);                                      // for use sqrt() --> functions we should need to declare math.h functon in header file
+        printf("Square root is %f", result);
+    return 0;
+}
