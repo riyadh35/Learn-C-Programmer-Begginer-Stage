f3acae03f845f98e85757e10cfea06873e95c157
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-19 23:52:40 +0600

    static Variable

--- cStorage.c
+++ cStorage.c
@@ -16,18 +16,34 @@ int main (void){
 // global variable 
 
 
+// void display();
+
+// int n= 5; // global variable 
+
+// int main(){
+//     ++n;
+//     display();
+//     return 0;
+// }
+
+// void display(){
+//     ++n;
+//     printf("n = %d", n);
+// }
+/* Suppose, a global variable is declared in file1. If you try to use that variable in
+ a different file file2, the compiler will complain. To solve this problem, keyword 
+extern is used in file2 to indicate that the external variable is declared in another file.
+ */
+
 void display();
 
-int n= 5; // global variable 
-
 int main(){
-    ++n;
     display();
-    return 0;
+    display();
 }
 
 void display(){
-    ++n;
-    printf("n = %d", n);
+    static int c = 1;
+    c += 5;
+    printf("%d",c);
 }
-
