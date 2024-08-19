34fdda2cf15e634d2de3c663eb356b9e2c8951f8
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-19 23:24:02 +0600

    global variable

--- cStorage.c
+++ cStorage.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 // Local Variable 
-
+// int i;
+/* 
 int main (void){
     for(int i = 0; i <5 ; ++i){
         printf("C Programming");
@@ -10,4 +11,23 @@ int main (void){
 
     printf("%d", i);
     return 0;
+} */
+
+// global variable 
+
+
+void display();
+
+int n= 5; // global variable 
+
+int main(){
+    ++n;
+    display();
+    return 0;
 }
+
+void display(){
+    ++n;
+    printf("n = %d", n);
+}
+
