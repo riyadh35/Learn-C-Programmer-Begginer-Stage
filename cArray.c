53c1079a335fd3fa62eaac46648fec1929ae0b93
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-20 22:22:55 +0600

    Program to take 5 values from the user and store them in an array

---
+++ cArray.c
@@ -0,0 +1,78 @@
+// An Array is a variable that can store multiple values.
+
+/* Few keynotes:
+Arrays have 0 as the first index, not 1. In this example, mark[0] is the first element.
+If the size of an array is n, to access the last element, the n-1 index is used. In this example, mark[4]
+Suppose the starting address of mark[0] is 2120d. Then, the address of the mark[1] will be 2124d. Similarly, the address of mark[2] will be 2128d and so on.
+This is because the size of a float is 4 bytes. */
+
+
+/* Change Value of Array elements  */
+
+// int mark[5] = {19, 10, 8, 17, 9};
+
+// make the value of the third element to -1
+
+// mark[2] = {-1};
+
+// make the value of the fifth element to 0
+
+// mark[4] = {0};
+
+
+// Input and OutPut Array Elements 
+
+// take input and store it in the 3rd Element
+
+// scanf("%d", &mark[2]);
+
+/* take input and store it in the ith element  */
+// scanf("%d", &mark[i-1]);
+
+// Here's how you can print an individual element of an array.
+
+// print the first element of the array
+/* printf("%d", mark[0]); */
+
+// print the third element of the array
+/* printf("%d", mark[2]);*/
+
+
+// print ith element of the array
+/* printf("%d", mark[i-1]); */
+
+
+
+// Example : 1 for Array input and Output 
+
+
+// Program to take 5 values from the user and store them in an array 
+// Print the elements stored in the Array
+
+
+
+
+#include <stdio.h>
+
+int main(){
+
+    int values[5];
+    printf("Enter 5 intergers: ");
+
+    // Taking input and storing it in an array
+
+    for (int i = 0 ; i <5 ; ++i){
+
+        scanf("%d", &values[i]);
+    }
+
+    printf("Displaying intergers: ");
+
+    // printing elements of an array
+
+    for(int i = 0; i < 5; ++i){
+        printf("%d\n", values[i]);
+    }
+
+    return 0;
+}
