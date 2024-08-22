babb9e01906f44d65115abfc36e26c2b066ba112
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-23 00:20:15 +0600

    Program to calculate the sum of array elements by passing to a function

--- passArraytoFunction.c
+++ passArraytoFunction.c
@@ -4,7 +4,7 @@
 
 /* Example 1: Pass Individual Arrays Elements  */
 
-#include <stdio.h>
+/* #include <stdio.h>
 
 void display(int age1, int age2){
     printf("%d\n", age1);
@@ -19,4 +19,35 @@ int main(){
     display(ageArrayp[2], ageArrayp[3]);
 
     return 0;
+} */
+
+
+
+
+/* Example 2: Pass Individual Array to Functions */
+
+// Program to calculate the sum of array elements by passing to a function
+
+#include <stdio.h>
+
+float calculateSum(float num[]);
+
+int main(){
+    float result, num[] = {23.4 , 55, 33.6, 3, 40.5, 18};
+
+    // num array is passed to calculateSum()
+
+    result = calculateSum(num);
+    printf("Result = %.2lf", result);
+    return 0;     
 }
+
+float calculateSum(float num[]){
+    float sum = 0.0;
+    for (int i = 0 ; i < 6; ++i){
+        sum += num[i];
+    }
+    return sum;
+}
+
+
