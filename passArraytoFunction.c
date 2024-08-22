9f5551cefc69707b0ee2285c615c9d4bdc85a494
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-23 00:28:02 +0600

    Pass Two Dimensioanl Arrays

--- passArraytoFunction.c
+++ passArraytoFunction.c
@@ -28,26 +28,60 @@ int main(){
 
 // Program to calculate the sum of array elements by passing to a function
 
+// #include <stdio.h>
+
+// float calculateSum(float num[]);
+
+// int main(){
+//     float result, num[] = {23.4 , 55, 33.6, 3, 40.5, 18};
+
+//     // num array is passed to calculateSum()
+
+//     result = calculateSum(num);
+//     printf("Result = %.2lf", result);
+//     return 0;     
+// }
+
+// float calculateSum(float num[]){
+//     float sum = 0.0;
+//     for (int i = 0 ; i < 6; ++i){
+//         sum += num[i];
+//     }
+//     return sum;
+// }
+
+
+
+
+ /* Example 3: pass two-dimensional arrays */
+
+
 #include <stdio.h>
+ void displayNumbers (int num[2][2]);
 
-float calculateSum(float num[]);
+ int main(){
 
-int main(){
-    float result, num[] = {23.4 , 55, 33.6, 3, 40.5, 18};
-
-    // num array is passed to calculateSum()
-
-    result = calculateSum(num);
-    printf("Result = %.2lf", result);
-    return 0;     
-}
-
-float calculateSum(float num[]){
-    float sum = 0.0;
-    for (int i = 0 ; i < 6; ++i){
-        sum += num[i];
+    int num[2][2];
+    printf("Enter 4 numbers: \n");
+    for(int i = 0; i < 2; ++i){
+        for(int j = 0; j < 2; ++j){
+            scanf("%d", &num[i][j]);
+        }
     }
-    return sum;
-}
 
+    // pass multi-dimensional array to a function 
 
+    displayNumbers(num);
+
+    return 0;
+ }
+
+ void displayNumbers(int num[2][2]){
+    printf("Displaying : \n ");
+
+    for(int i = 0; i < 2; ++i){
+        for(int j = 0; j < 2; ++j){
+            printf("%d ", num[i][j]);
+        }
+    }
+ }
