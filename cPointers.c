667dc0c788ee629a277e5cdfa9eb8029defa5ae8
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-24 00:29:46 +0600

    Pointer

--- cPointers.c
+++ cPointers.c
@@ -4,14 +4,25 @@
 #include <stdio.h>
 
 int main(){
-    int var = 5; 
+   /*  int var = 5; 
     printf("var = %d\n", var);
 
 
     // Notice the user of & before var
-
     printf("address of var : %p", &var);
 
 
+    return 0; */
+
+    int* pc, c; 
+    c = 5;
+
+    pc = &c;
+    *pc = 1;
+    
+    printf("%d ", *pc); // Output : 1
+    printf("%d",c); // output: 1
+
     return 0;
 }
+
