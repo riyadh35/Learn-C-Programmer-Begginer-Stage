8851e2d7049046b329cfa99ffe268aa8fc6f8b5d
riyadh35 <mdriyadhossain356@gmail.com>
2024-08-27 23:25:18 +0600

    Pass address as argument to function with pointers.

---
+++ passArgumenttoFunction.c
@@ -0,0 +1,4 @@
+//It is also possible to pass addresses as arguments to functions . 
+// To accept these addresses in the function definition, we can use pointers. It's because pointers are used to store addresses.
+
+
