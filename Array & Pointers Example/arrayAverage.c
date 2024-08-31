798bcc28cda2f697eb01d36c528332d2b84a8fca
riyadh35 <mdriyadhossain356@gmail.com>
2024-09-01 02:52:34 +0600

    Calculate the average of Array elements

---
+++ Array & Pointers Example/arrayAverage.c
@@ -0,0 +1,27 @@
+/* Calculate the average of Array Elements. */
+
+#include <stdio.h>
+
+int main(){
+    int n, i;
+    float num[100], sum = 0.0, avg;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    while (n > 100 || n < 1){
+        printf("Error ! Enter a wrong elements numbers(1 to 100) \n");
+        printf("Enter the number again");
+        scanf("%d", &n);
+    }
+
+    for(i = 0; i < n; i++){
+        printf("%d.Enter the number: ", i + 1);
+        scanf("%f", &num[i]);
+        sum += num[i];
+    }
+
+    avg = sum / n;
+    printf("Average = %.2f", avg);
+    return 0 ;
+}
