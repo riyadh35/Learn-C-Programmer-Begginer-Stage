40e6299bf4b8125eaad06609bb660743cbb4705e
riyadh35 <mdriyadhossain356@gmail.com>
2024-09-18 00:53:05 +0600

    Storing the largest number of elements in an array

--- Array & Pointers Example/calculateAverage.c
+++ Array & Pointers Example/calculateAverage.c
@@ -26,32 +26,74 @@
 // }
 
 
+// #include <stdio.h>
+
+// int main(){
+
+//     int n , i;
+//     float num[100], sum = 0.0, avg;
+
+//     printf("Enter the elements of numbers: ");
+//     scanf("%d", &n);
+
+
+//     while (n > 100 || n < 1)
+//     {
+//       printf("Error ! number should in range of (1 to 100). \n");
+//       printf("Enter the numbers again : ");
+//       scanf("%d", &n);
+//     }
+
+//     for(i = 0 ; i < n; ++i ){
+//         printf("%d. Enter the number: ", i + 1 );
+//         scanf("%f. Average numbers of ", &num[i]);
+//         sum += num[i];
+//     }
+    
+
+//     avg = sum /n ;
+//     printf("%f. the average %.2f :", avg);
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+/* -----------------------C Program to Find Largest Element in an Array----------------------------------   */
+
+
+
 #include <stdio.h>
 
 int main(){
+    int n;
+    double arr[100];
 
-    int n , i;
-    float num[100], sum = 0.0, avg;
-
-    printf("Enter the elements of numbers: ");
+    printf("Enter the elements numbers (1 to 100): ");
     scanf("%d", &n);
 
-
-    while (n > 100 || n < 1)
-    {
-      printf("Error ! number should in range of (1 to 100). \n");
-      printf("Enter the numbers again : ");
-      scanf("%d", &n);
+    for(int i = 0; i < n; ++i){
+    printf("Enter number %d:", i + 1);
+    scanf("%lf", &arr[i]);
     }
 
-    for(i = 0 ; i < n; ++i ){
-        printf("%d. Enter the number: ", i + 1 );
-        scanf("%f. Average numbers of ", &num[i]);
-        sum += num[i];
-    }
-    
 
-    avg = sum /n ;
-    printf("%f. the average %.2f :", avg);
+    // storing the largest number to arr[0]
+
+    for(int i = 1; i < n; ++i){
+    if(arr[0] < arr[i]){
+      arr[0] = arr[i];
+    }
+  }
+
+
+    printf("Largest number is = %.2lf", arr[0]);
+
     return 0;
 }
