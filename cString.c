3830f91e365fb0587189b4c23380da651aaf351c
riyadh35 <mdriyadhossain356@gmail.com>
2024-09-23 23:54:31 +0600

    C programming fgets() and puts() function. for read string use fgets() function & puts() function  to display string

--- cString.c
+++ cString.c
@@ -7,13 +7,31 @@ Read String from the User */
 // In c programming we can use the scanf() funciton to read the string..
 
 
+// #include <stdio.h>
+
+// int main(){
+
+//     char name[20];
+//     printf("Enter a name: ");
+//     scanf("%s", name);
+//     printf("Your name is %s.", name);
+//     return 0;
+// }
+
+/* How to read a line of text?
+You can use the fgets() function to read a line of string. And, you can use puts() to display the string */
+
+
+/* Example 2: fgets() and puts() */
+
 #include <stdio.h>
 
 int main(){
+    char name[30];
+    printf("Enter name: ");
+    fgets(name, sizeof(name), stdin);  // read string
+    printf("Name: ");
 
-    char name[20];
-    printf("Enter a name: ");
-    scanf("%s", name);
-    printf("Your name is %s.", name);
+    puts(name);    // display string
     return 0;
 }
